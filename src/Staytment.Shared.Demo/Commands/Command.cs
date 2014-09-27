efef4df73655920343c24cb0783a5d77f53b7dac
Niklas Mollenhauer <nikeee@outlook.com>
2014-09-27 20:29:43 +0200

    Add commands

---
+++ src/Staytment.Shared.Demo/Commands/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Staytment.Shared.Demo.Commands
+{
+    internal abstract class Command : ICommand
+    {
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
+
+
+        public event EventHandler CanExecuteChanged;
+
+        protected void OnCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler(this, new EventArgs());
+        }
+    }
+}
---
+++ src/Staytment.Shared.Demo/Commands/CreatePostCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Staytment.Shared.Demo.Commands
+{
+    internal class CreatePostCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
